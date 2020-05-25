fb5c9e139e1a2501f2500769eaebede8226c5081
Raindrop <qq727474430@gmail.com>
2020-05-25 16:15:45 +0800

    Update dingtalk function

--- src/org/devops/tool.groovy
+++ src/org/devops/tool.groovy
@@ -26,10 +26,17 @@ def DingTalkNotify(webhook, jobName, status, version) {
         def url = "https://oapi.dingtalk.com/robot/send?access_token=${webhook}"
         def log = getChangeString()
         def data = """{
-            "type": "MARKDOWN",
-            "title": "构建测试",
-            "text": "### 构建信息\\n>- 应用名称: **${jobName}**\\n- 构建结果: **${status}**\\n- 当前版本: **${version}**\\n- 构建发起: 
-            **${env.BUILD_USER}**\\n- 持续时间: **${currentBuild.durationString}**\\n- 构建日志: [点击查看详情](${env.BUILD_URL}console)\\n#### 更新记录: \\n${log}"
+            "msgtype": "markdown",
+            "markdown": {
+                "title": "监控报警",
+                "text": "### 构建信息\n>- 应用名称: **${jobName}**\n- 构建结果: **${status}**\n- 当前版本: **${version}**\n- 构建发起: **${env.BUILD_USER}**\n- 持续时间: **${currentBuild.durationString}**\n- 构建日志: [点击查看详情](${env.BUILD_URL}console)\n#### 更新记录: \n${log}"
+            },
+            "at": {
+                "atMobiles": [
+                    "18215518314"
+                ], 
+                "isAtAll": false
+            }
         }"""
 
         httpRequest acceptType: 'APPLICATION_JSON_UTF8',
@@ -44,3 +51,12 @@ def DingTalkNotify(webhook, jobName, status, version) {
     }
 }
 
+
+def MailNotify(to, subject, body) {
+    wrap([$class: 'BuildUser']) {
+        mail to: "xxxxx@163.com",
+                subject: "PineLine '${JOB_NAME}' (${BUILD_NUMBER}) result",
+                body: "${env.BUILD_USER}'s Pineline '${JOB_NAME}' (${BUILD_NUMBER}) run success\n 请及时前往 ${env.BUILD_URL} 进行查看"
+        echo "${env.BUILD_USER}'s Pineline '${JOB_NAME}' (${BUILD_NUMBER}) run success\n 请及时前往 ${env.BUILD_URL} 进行查看"
+    }
+}
