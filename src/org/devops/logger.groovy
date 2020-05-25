e0786546f4144e3ac585bb3f300e89b7915b8413
Raindrop <qq727474430@gmail.com>
2020-05-25 15:39:14 +0800

    Update logger shared lib

--- src/org/devops/logger.groovy
+++ src/org/devops/logger.groovy
@@ -1,14 +1,17 @@
 package org.devops
 
-/**
- * RED_COLOR='\E[1;31m'   #红
- * GREEN_COLOR='\E[1;32m' #绿
- * YELOW_COLOR='\E[1;33m' #黄
- * BLUE_COLOR='\E[1;34m'  #蓝
- * PINK='\E[1;35m'        #粉红
- * RES='\E[0m'
- */
+/*
+   ##################################################
+   #                                                #
+   #            Logger shared library               #
+   #               RED == \033[1;31m                #
+   #               GREEN == \E[1;32m                #
+   #               BLUE == \E[1;34m                 #
+   #               PINK == \E[1;35m                 #
+   ##################################################
+*/
 
+// Info log
 def Info(message) {
     def colorMsg = "\033[1;32m >>>>>>>>>>>>>>${message}<<<<<<<<<<<<<<"
     ansiColor('xterm') {
@@ -16,17 +19,18 @@ def Info(message) {
     }
 }
 
+// Warning log
 def Warning(message) {
-    def colorMsg = "\\033[1;33m >>>>>>>>>>>>>>${message}<<<<<<<<<<<<<<"
+    def colorMsg = "\\033[1;35m >>>>>>>>>>>>>>${message}<<<<<<<<<<<<<<"
     ansiColor('xterm') {
         println(colorMsg)
     }
 }
 
+// Error log
 def Error(message) {
     def colorMsg = "\033[1;31m >>>>>>>>>>>>>>${message}<<<<<<<<<<<<<<"
     ansiColor('xterm') {
         println(colorMsg)
     }
 }
-
