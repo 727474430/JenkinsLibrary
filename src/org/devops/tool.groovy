0c74509c3857ceeeff178a4aa38429bc1da0ef30
Raindrop <qq727474430@gmail.com>
2020-05-25 17:52:12 +0800

    Email notification function

--- src/org/devops/tool.groovy
+++ src/org/devops/tool.groovy
@@ -9,6 +9,7 @@ package org.devops
    DingTalk webSite: https://ding-doc.dingtalk.com/doc#/serverapi2/elzz1p/yCEes
 */
 
+// DingTalk job complete notification
 def DingTalkNotify(webhook, jobName, status, version) {
     wrap([$class: 'BuildUser']) {
         def url = "https://oapi.dingtalk.com/robot/send?access_token=${webhook}"
@@ -17,7 +18,7 @@ def DingTalkNotify(webhook, jobName, status, version) {
             "msgtype": "markdown",
             "markdown": {
                 "title":"监控报警",
-                "text": "### 构建信息\n>- 应用名称: **${jobName}**\n>- 构建结果: **${status}**\n>- 当前版本: **${version}**\n>- 构建发起: **${env.BUILD_USER}**\n>- 持续时间: **${currentBuild.durationString}**\n>- 构建日志: [点击查看详情](${env.BUILD_URL}console)\n#### 更新记录: \\n>-${log}"
+                "text": "### 构建信息\n>- 应用名称: **${jobName}**\n>- 构建结果: **${status}**\n>- 当前版本: **${version}**\n>- 构建发起: **${env.BUILD_USER}**\n>- 持续时间: **${currentBuild.durationString}**\n>- 构建日志: [点击查看详情](${env.BUILD_URL}console)\n#### 更新记录: \n>- ${log}"
             },
             "at": {
                 "atMobiles": [
@@ -39,15 +40,16 @@ def DingTalkNotify(webhook, jobName, status, version) {
     }
 }
 
-def MailNotify(to, subject, body) {
+// Email job complete notification
+def MailNotify(to, status) {
     wrap([$class: 'BuildUser']) {
-        mail to: "xxxxx@163.com",
+        mail to: "${to}",
                 subject: "PineLine '${JOB_NAME}' (${BUILD_NUMBER}) result",
-                body: "${env.BUILD_USER}'s Pineline '${JOB_NAME}' (${BUILD_NUMBER}) run success\n 请及时前往 ${env.BUILD_URL} 进行查看"
-        echo "${env.BUILD_USER}'s Pineline '${JOB_NAME}' (${BUILD_NUMBER}) run success\n 请及时前往 ${env.BUILD_URL} 进行查看"
+                body: "${env.BUILD_USER}'s Pineline '${JOB_NAME}' (${BUILD_NUMBER}) run ${status}\n 请及时前往 ${env.BUILD_URL} 进行查看"
     }
 }
 
+// Get commit log
 def getChangeString() {
     def changeString = ""
     def MAX_MSG_LEN = 20
