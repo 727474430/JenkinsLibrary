67904a09eb3bef06f5c5da3afc685833f5c0ac50
Raindrop <qq727474430@gmail.com>
2020-05-25 17:19:24 +0800

    æUpdate dingtalk

--- src/org/devops/tool.groovy
+++ src/org/devops/tool.groovy
@@ -1,5 +1,50 @@
 package org.devops
 
+/*
+   ##################################################
+   #                                                #
+   #         Message Notify shared library          #
+   #                                                #
+   ##################################################
+   DingTalk webSite: https://ding-doc.dingtalk.com/doc#/serverapi2/elzz1p/yCEes
+*/
+
+def DingTalkNotify(webhook, jobName, status, version) {
+    wrap([$class: 'BuildUser']) {
+        def url = "https://oapi.dingtalk.com/robot/send?access_token=${webhook}"
+        def log = getChangeString()
+        def data = """{
+            "msgtype": "markdown",
+            "markdown": {
+                "title":"监控报警",
+                "text": "### 构建信息\n>- 应用名称: **${jobName}**\n- 构建结果: **${status}**\n- 当前版本: **${version}**\n- 构建发起: **${env.BUILD_USER}**\n- 持续时间: **${currentBuild.durationString}**\n- 构建日志: [点击查看详情](${env.BUILD_URL}console)\n#### 更新记录: \\n${log}"
+            },
+            "at": {
+                "isAtAll": true
+            }
+        }"""
+
+        httpRequest acceptType: 'APPLICATION_JSON_UTF8',
+                consoleLogResponseBody: false,
+                contentType: 'APPLICATION_JSON_UTF8',
+                httpMode: 'POST',
+                ignoreSslErrors: true,
+                requestBody: data,
+                responseHandle: 'NONE',
+                url: "${url}",
+                quiet: true
+    }
+}
+
+def MailNotify(to, subject, body) {
+    wrap([$class: 'BuildUser']) {
+        mail to: "xxxxx@163.com",
+                subject: "PineLine '${JOB_NAME}' (${BUILD_NUMBER}) result",
+                body: "${env.BUILD_USER}'s Pineline '${JOB_NAME}' (${BUILD_NUMBER}) run success\n 请及时前往 ${env.BUILD_URL} 进行查看"
+        echo "${env.BUILD_USER}'s Pineline '${JOB_NAME}' (${BUILD_NUMBER}) run success\n 请及时前往 ${env.BUILD_URL} 进行查看"
+    }
+}
+
 def getChangeString() {
     def changeString = ""
     def MAX_MSG_LEN = 20
@@ -17,46 +62,4 @@ def getChangeString() {
         changeString = " - No new changes"
     }
     return (changeString)
-}
-
-// 构建成功 ✅
-// 构建失败 ❌
-def DingTalkNotify(webhook, jobName, status, version) {
-    wrap([$class: 'BuildUser']) {
-        def url = "https://oapi.dingtalk.com/robot/send?access_token=${webhook}"
-        def log = getChangeString()
-        def data = """{
-            "msgtype": "markdown",
-            "markdown": {
-                "title": "监控报警",
-                "text": "### 构建信息\n>- 应用名称: **${jobName}**\n- 构建结果: **${status}**\n- 当前版本: **${version}**\n- 构建发起: **${env.BUILD_USER}**\n- 持续时间: **${currentBuild.durationString}**\n- 构建日志: [点击查看详情](${env.BUILD_URL}console)\n#### 更新记录: \n${log}"
-            },
-            "at": {
-                "atMobiles": [
-                    "18215518314"
-                ], 
-                "isAtAll": false
-            }
-        }"""
-
-        httpRequest acceptType: 'APPLICATION_JSON_UTF8',
-                consoleLogResponseBody: false,
-                contentType: 'APPLICATION_JSON_UTF8',
-                httpMode: 'POST',
-                ignoreSslErrors: true,
-                requestBody: data,
-                responseHandle: 'NONE',
-                url: "${url}",
-                quiet: true
-    }
-}
-
-
-def MailNotify(to, subject, body) {
-    wrap([$class: 'BuildUser']) {
-        mail to: "xxxxx@163.com",
-                subject: "PineLine '${JOB_NAME}' (${BUILD_NUMBER}) result",
-                body: "${env.BUILD_USER}'s Pineline '${JOB_NAME}' (${BUILD_NUMBER}) run success\n 请及时前往 ${env.BUILD_URL} 进行查看"
-        echo "${env.BUILD_USER}'s Pineline '${JOB_NAME}' (${BUILD_NUMBER}) run success\n 请及时前往 ${env.BUILD_URL} 进行查看"
-    }
 }
